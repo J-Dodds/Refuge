214ed6e15683be03bb17b3fb751d5e73983f40f4
J-Dodds <1004254@student.sae.edu.au>
2017-07-10 18:04:31 +1000

    Refugee object starts at first node in array

---
+++ Refuge/Assets/Scripts/MapNavigation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNavigation : MonoBehaviour
+{
+
+    public GameObject[] mapNodes;
+    public GameObject refugeeObject;
+
+    private int currentLocation = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Error Logs
+		if (mapNodes.Length < 0)
+        {
+            Debug.Log("No Nodes added");
+        }
+
+        if (refugeeObject == null)
+        {
+            Debug.Log("No Refugee Object");
+        }
+
+        //Set start point
+        refugeeObject.transform.position = mapNodes[0].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
+
+
+//Check what nodes current node is connected to. if it is move to the node on a click.
