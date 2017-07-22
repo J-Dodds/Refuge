ec895798914e3f1e0ba3ba7cebb23c38c9a2aa11
Jacob Duniam <duniamjj@gmail.com>
2017-07-22 11:44:30 +1000

    - Added the Options Menu into my scene  - Main Menu, Options, Credits menus now work

---
+++ Refuge/Assets/Scripts/UI_Functions/UI_MenuTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MenuTransition : MonoBehaviour {
+	public GameObject destinationPanel;
+	public GameObject thisPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick()
+	{
+		//Turn on the next scene and off this one
+		thisPanel.SetActive(false);
+		destinationPanel.SetActive(true);
+	}
+}
