a0d8f5cc7bb8c7a3688d67358109ec70fdfb9ce6
Jacob Duniam <duniamjj@gmail.com>
2017-07-31 15:30:46 +1000

    - Moved scenes into a nice new Prototype 2 scene  - using the Canvas prefabs from before to create the menu system for this new prototype  - Added Alex's characters from the prototype before  - Created a Prompt system and object for informing players of decisions  - Added a cute background shade Object

---
+++ Refuge/Assets/PromptMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PromptMenu : MonoBehaviour {
+	public Text headerContent;
+	public Text bodyContent;
+
+	public bool PromptToggle;
+
+	public GameObject Prompt;
+
+	public static PromptMenu instance;
+
+	public void Awake()
+	{
+		instance = this;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.P))
+			DisplayPrompt("TestPrompt", "This is a test prompt");
+		
+	}
+
+	public void DisplayPrompt(string header, string body)
+	{
+		Debug.Log("Trying to display the Prompt");
+		// Set the text and activate the promot
+		headerContent.text = header;
+		bodyContent.text = body;
+
+		PromptToggle = true;
+
+		TogglePrompt();
+	}
+
+	public void TogglePrompt()
+	{
+		if(PromptToggle)
+			Prompt.SetActive(false);
+		else
+			Prompt.SetActive(true);
+	}
+}
