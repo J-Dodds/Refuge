47e653cadfdfe14bcfeb30c26b058568e8f81064
Alex Luthe <1005745@student.sae.edu.au>
2017-07-31 20:16:43 +1000

    Filled ItemType enum from the object/interaction spreadsheet. Fixed the Char_r prefab

--- Refuge/Assets/Scripts/RevisedScripts/Item_r.cs
+++ Refuge/Assets/Scripts/RevisedScripts/Item_r.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class Item_r : MonoBehaviour {
 
     public enum ItemType {
-
+        ITCannedBeans = 0,
+        ITDriedFruit = 1,
+        ITMealRation = 2,
+        ITPureWater = 3,
+        ITDirtyWater = 4,
+        ITHandheldPuzzle = 5,
+        ITNovel = 6,
+        ITDessert = 7,
+        ITFirstAid = 8,
+        ITMedicine = 9,
+        
+        ITInteravenousFluids = 10,
+        ITMetronidazole = 11,
+        ITPenicilin = 12,
+        ITRepairFracture = 13,
     };
 
     public ItemType itemType;
