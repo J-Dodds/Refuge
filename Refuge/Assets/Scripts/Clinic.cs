66d94132bf512e0edf26930f989e33cfd7378947
J-Dodds <1004254@student.sae.edu.au>
2017-08-07 16:56:38 +1000

    Clinic now does disease stuff. Cost to cure depends on the amount of diseases

--- Refuge/Assets/Scripts/Clinic.cs
+++ Refuge/Assets/Scripts/Clinic.cs
@@ -10,28 +10,72 @@ public class Clinic : Locations
     public GameManager_r gameManager;
 
     public int healCost = 100;
-    public int cureCost = 200;
+    public int cureCost = 0;
+
+    Text healText;
+    Text cureText;
+
+    bool injured = false;
+    bool cholera = false;
+    bool dysentary = false;
+    bool typhoid = false;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        healText.text = "Restore Health = $" + healCost;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (character.GetComponent<Character_r>().injured == true)
+        {
+            if (injured == false)
+            {
+                cureCost += 100;
+                injured = true;
+            }
+        }
+
+        if (character.GetComponent<Character_r>().cholera == true)
+        {
+            if (cholera == false)
+            {
+                cureCost += 100;
+                cholera = true;
+            }
+        }
+
+        if (character.GetComponent<Character_r>().dysentery == true)
+        {
+            if (dysentary == false)
+            {
+                cureCost += 100;
+                dysentary = true;
+            }
+        }
+
+        if(character.GetComponent<Character_r>().typhoid == true)
+        {
+            if (injured == false)
+            {
+                cureCost += 100;
+                typhoid = true;
+            }
+        }
+
+        cureText.text = "Cure Disease = $" + cureCost;
+    }
 
     public void HealRefugee()
     {
-        if (character.GetComponent<Refugees>().health >= 100)
+        if (character.GetComponent<Character_r>().health >= 100)
         {
             if (gameManager.partyMoney >= healCost)
             {
-                character.GetComponent<Refugees>().health += healAmount;
-                Debug.Log(character.GetComponent<Refugees>().health);
+                character.GetComponent<Character_r> ().health += healAmount;
+                Debug.Log(character.GetComponent<Character_r>().health);
                 gameManager.partyMoney -= healCost;
             }
         }
@@ -39,13 +83,20 @@ public class Clinic : Locations
 
     public void CureDisease()
     {
-        if(character.GetComponent<Refugees>().isSick == true)
+        if(character.GetComponent<Character_r>().injured == true || character.GetComponent<Character_r>().cholera == true || character.GetComponent<Character_r>().dysentery == true || character.GetComponent<Character_r>().typhoid == true)
         {
             if(gameManager.partyMoney >= cureCost)
             {
-                character.GetComponent<Refugees>().isSick = false;
+                character.GetComponent<Character_r>().injured = false;
+                character.GetComponent<Character_r>().cholera = false;
+                character.GetComponent<Character_r>().dysentery = false;
+                character.GetComponent<Character_r>().typhoid = false;
                 gameManager.partyMoney -= cureCost;
 
+                injured = false;
+                cholera = false;
+                dysentary = false;
+                typhoid = false;
             }
         }
     }
