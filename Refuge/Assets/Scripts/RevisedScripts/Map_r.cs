54ab83cb999d06ef9c8c5fc012cec54661148779
J-Dodds <1004254@student.sae.edu.au>
2017-08-07 19:55:39 +1000

    UI text now appears when a character catches something while traveling

--- Refuge/Assets/Scripts/RevisedScripts/Map_r.cs
+++ Refuge/Assets/Scripts/RevisedScripts/Map_r.cs
@@ -25,40 +25,16 @@ public class Map_r : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (newLocation) {
-        if (Vector3.Distance(refugeeObj.transform.position, newLocation.transform.position) > 0.6) {
-            refugeeObj.transform.position = Vector3.Lerp(refugeeObj.transform.position, newLocation.transform.position, Time.deltaTime * GM.partySpeed);
-                foreach (GameObject chara in GM.characters) {
+        if (newLocation)
+        {
+        if (Vector3.Distance(refugeeObj.transform.position, newLocation.transform.position) > 0.6)
+            {
+                refugeeObj.transform.position = Vector3.Lerp(refugeeObj.transform.position, newLocation.transform.position, Time.deltaTime * GM.partySpeed);
+
+                foreach (GameObject chara in GM.characters)
+                {
                     chara.GetComponent<Character_r>().AddHunger(-0.002f);
                     chara.GetComponent<Character_r>().AddThirst(-0.002f);
-
-                    int rand = Random.Range(0, 100);
-                    if(rand <= chanceOfNothing)
-                    {
-                        Debug.Log("You caught nothing");
-                    }
-                    else if(rand <= chanceOfInjury)
-                    {
-                        chara.GetComponent<Character_r>().injured = true;
-                        chara.GetComponent<Character_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten injured! ");
-                    }
-                    else if(rand <= chanceOfCholera)
-                    {
-                        chara.GetComponent<Character_r>().cholera = true;
-                        chara.GetComponent<Character_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten cholera! ");
-                    }
-                    else if (rand <= chanceOfDysentary)
-                    {
-                        chara.GetComponent<Character_r>().cholera = true;
-                        chara.GetComponent<Character_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten dysentary! ");
-                    }
-                    else if(rand <= chanceOfTyphoid)
-                    {
-                        chara.GetComponent<Character_r>().typhoid = true;
-                        chara.GetComponent<Character_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten typhoid! ");
-                    }
-
-                    StartCoroutine(chara.GetComponent<Character_r>().HasGottenHealthCondition());
                 }
             }
         else {
@@ -66,6 +42,36 @@ public class Map_r : MonoBehaviour {
                 refugeeObj.transform.position = new Vector3(newLocation.transform.position.x + movementXOffset, newLocation.transform.position.y + movementYOffset, -5);
                 newLocation.GetComponent<Location_r>().Scavenge();
                 newLocation = null;
+
+                foreach (GameObject chara in GM.characters)
+                {
+                    int rand = Random.Range(0, 100);
+                    if (rand <= chanceOfNothing)
+                    {
+                        Debug.Log("You caught nothing");
+                    }
+                    else if (rand > chanceOfNothing && rand <= chanceOfInjury)
+                    {
+                        chara.GetComponent<Character_r>().injured = true;
+                        GM.GetComponent<GameManager_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten injured! ");
+                    }
+                    else if (rand > chanceOfInjury && rand <= chanceOfCholera)
+                    {
+                        chara.GetComponent<Character_r>().cholera = true;
+                        GM.GetComponent<GameManager_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten cholera! ");
+                    }
+                    else if (rand > chanceOfCholera && rand <= chanceOfDysentary)
+                    {
+                        chara.GetComponent<Character_r>().dysentery = true;
+                        GM.GetComponent<GameManager_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten dysentary! ");
+                    }
+                    else if (rand > chanceOfDysentary && rand <= chanceOfTyphoid)
+                    {
+                        chara.GetComponent<Character_r>().typhoid = true;
+                        GM.GetComponent<GameManager_r>().conditionReportText.text += (chara.GetComponent<Character_r>().charName + " has gotten typhoid! ");
+                    }
+                }
+                StartCoroutine(GM.GetComponent<GameManager_r>().HasGottenHealthCondition());
             }
         }
 	}
