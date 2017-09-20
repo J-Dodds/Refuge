b64db611c73685ae6748ef7781998aed381295bf
J-Dodds <1004254@student.sae.edu.au>
2017-09-20 20:06:25 +1000

    Travel confirmation screen working again, someone had broken it in code by putting it in an if statement that required that it be activated

--- Refuge/Assets/Scripts/RevisedScripts/Map_r.cs
+++ Refuge/Assets/Scripts/RevisedScripts/Map_r.cs
@@ -83,10 +83,8 @@ public class Map_r : MonoBehaviour {
             time = (int)Vector3.Distance(refugeeObj.transform.position, location.transform.position) / 5;
 
             newLocation = location;
-            if (confirm)
-                confirmTravelPanel.SetActive(true);
-            else
-                YesTravel();
+            confirmTravelPanel.SetActive(true);
+
             costOfTravelText.text = "You Will Lose: Health - " + (((100.0f - hungerPercentLeft) / 1000000.0f) + ((100f - thirstPercentLeft) / 1000000f) + ((100f - stressPercentLeft) / 1000000f)) * (((newLocation.transform.position.x - refugeeObj.transform.position.x) + (newLocation.transform.position.y - refugeeObj.transform.position.y)) * Time.deltaTime * GM.partySpeed) + "\n" +
                     "                   Hunger - " + time * 0.0001f * (((newLocation.transform.position.x - refugeeObj.transform.position.x) + (newLocation.transform.position.y - refugeeObj.transform.position.y)) * Time.deltaTime * GM.partySpeed) + "\n" +
                     "                   Thirst - " + time * 0.0001f * (((newLocation.transform.position.x - refugeeObj.transform.position.x) + (newLocation.transform.position.y - refugeeObj.transform.position.y)) * Time.deltaTime * GM.partySpeed) + "\n" +
