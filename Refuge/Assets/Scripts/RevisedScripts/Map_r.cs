5e4bc9975d9a1f610846b1949d4b4e050a524776
J-Dodds <1004254@student.sae.edu.au>
2017-09-20 20:01:29 +1000

    Fixed a bug relating to travel

--- Refuge/Assets/Scripts/RevisedScripts/Map_r.cs
+++ Refuge/Assets/Scripts/RevisedScripts/Map_r.cs
@@ -77,7 +77,7 @@ public class Map_r : MonoBehaviour {
 
     public void Travel(GameObject location)
     {
-        if (!newLocation || newLocation.GetComponent<Location_r>().possibleLocations.Contains(location)) {
+        if (newLocation != location || newLocation.GetComponent<Location_r>().possibleLocations.Contains(location)) {
         //if (location.GetComponent<Location_r>().locationNumber == currentLocationNumber - 1 || location.GetComponent<Location_r>().locationNumber == currentLocationNumber + 1)
         //{
             time = (int)Vector3.Distance(refugeeObj.transform.position, location.transform.position) / 5;
